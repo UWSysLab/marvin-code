5e764b24cc285606b78e30e7084a1ead812b64e1
Niel Lebeck <nl35@cs.washington.edu>
2018-10-03 12:00:17 -0700

    Add start delay argument for starting app clones

--- scripts/manage-clones.pl
+++ scripts/manage-clones.pl
@@ -6,12 +6,41 @@ use strict;
 my $PARENT_PACKAGE = "edu.washington.cs.nl35";
 my $CLONE_DIR = "/home/nl35/research/android-memory-model/temp";
 
-my $usage = "./manage-clones.pl [create|delete|start|stop] app-path num-clones";
-die $usage unless @ARGV == 3;
+my $usage =   "usage: ./manage-clones.pl {create|delete|start|stop} ...\n"
+            . "       ./manage-clones.pl create app-path num-clones\n"
+            . "       ./manage-clones.pl start  app-path num-clones start-delay\n"
+            . "       ./manage-clones.pl stop   app-path num-clones\n"
+            . "       ./manage-clones.pl delete app-path num-clones\n"
+            ;
 
-my ($cmd, $appPath, $numClones) = @ARGV;
+my ($cmd, $appPath, $numClones, $startDelay);
+
+$cmd = $ARGV[0];
 if (!($cmd =~ /^create|delete|start|stop$/)) {
     die $usage;
+}
+
+if ($cmd =~ /^create|delete|stop$/) {
+    if (@ARGV == 3) {
+        $appPath = $ARGV[1];
+        $numClones = $ARGV[2];
+        print("Running command $cmd; appPath $appPath, numClones $numClones\n");
+    }
+    else {
+        die $usage;
+    }
+}
+
+if ($cmd =~ /^start$/) {
+    if (@ARGV == 4) {
+        $appPath = $ARGV[1];
+        $numClones = $ARGV[2];
+        $startDelay = $ARGV[3];
+        print("Running command $cmd; appPath $appPath, numClones $numClones, startDelay $startDelay\n");
+    }
+    else {
+        die $usage;
+    }
 }
 
 my $appName = getAppNameFromPath($appPath);
@@ -57,6 +86,9 @@ sub deleteClones {
 sub startClones {
     for (my $i = 0; $i < $numClones; $i++) {
         system("adb shell am start -a android.intent.action.MAIN -n $PARENT_PACKAGE.$packageName$i/.MainActivity");
+        if ($i < $numClones - 1) {
+            sleep $startDelay;
+        }
     }
 }
 
