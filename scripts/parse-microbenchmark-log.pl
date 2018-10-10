254b1b0ae9fa8be0af1dd84a1ec2f22ed4ed3542
Niel Lebeck <nl35@cs.washington.edu>
2018-10-10 14:02:16 -0700

    Change log-parsing script to parse messages from 'am start'

--- scripts/parse-microbenchmark-log.pl
+++ scripts/parse-microbenchmark-log.pl
@@ -5,22 +5,29 @@ use strict;
 
 my $MICROBENCHMARK_NAME = "edu.washington.cs.nl35.microbenchmark";
 
+print("timestamp\tpid\tclone\tevent\n");
 while(<>) {
     if (/^(\d+)-(\d+)\s+(\d+):(\d+):(\d+)\.(\d+)\s+(\d+)\s+(\d+)\s+(\w+)\s+(\w+)\s*:\s*(.*)$/) {
         my ($month, $date, $hours, $minutes, $seconds, $milliseconds) = ($1, $2, $3, $4, $5, $6);
         my ($pid, $tid) = ($7, $8);
         my ($logLevel, $tag, $msg) = ($9, $10, $11);
         my $time = "$hours:$minutes:$seconds.$milliseconds";
-        if ($msg =~ /^Start proc (\d+):$MICROBENCHMARK_NAME\d+/) {
+        if ($msg =~ /^Start proc (\d+):$MICROBENCHMARK_NAME(\d+)/) {
             my $startedPid = $1;
-            print("$time\t$startedPid\tstart\n");
+            my $cloneNum = $2;
+            print("$time\t$startedPid\t$cloneNum\tstart\n");
         }
         if ($msg =~ /^MicroBenchmark onResume finished$/) {
-            print("$time\t$pid\tresume\n");
+            print("$time\t$pid\tN/A\tresume\n");
         }
-        if ($msg =~ /^Process $MICROBENCHMARK_NAME\d+ \(pid (\d+)\) has died$/) {
-            my $diedPid = $1;
-            print("$time\t$diedPid\tdie\n");
+        if ($msg =~ /^Process $MICROBENCHMARK_NAME(\d+) \(pid (\d+)\) has died$/) {
+            my $cloneNum = $1;
+            my $diedPid = $2;
+            print("$time\t$diedPid\t$cloneNum\tdie\n");
+        }
+        if ($msg =~ /^START u0 \{act=android\.intent\.action\.MAIN flg=0x10000000 cmp=$MICROBENCHMARK_NAME(\d+)\/\.MainActivity\} from uid \d+ on display \d+$/) {
+            my $cloneNum = $1;
+            print("$time\tN/A\t$cloneNum\tstart-intent\n");
         }
     }
 }
