35a991523f6faa2acdbf8409908f1b0a45441b01
Niel Lebeck <nl35@cs.washington.edu>
2018-11-02 09:31:09 -0700

    Add error-handling and soft-dirty bit-reading to wsetracker

--- pagemap-working-set-estimation/wsetracker.c
+++ pagemap-working-set-estimation/wsetracker.c
@@ -1,9 +1,64 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+#ifndef PAGE_SIZE
+#define PAGE_SIZE 4096
+#endif
+
+int pagemapFd = -1;
+
+int getBit(unsigned char word[8], int index) {
+    int byteIndex = index / 8;
+    int bitIndex = index % 8;
+    return (word[byteIndex] >> bitIndex) & 1;
+}
+
+int checkPageSoftDirty(unsigned int page) {
+    unsigned int offset = page * 8;
+    off_t retOffset = lseek(pagemapFd, offset, SEEK_SET);
+    if (retOffset < 0) {
+        perror("lseek on pagemap");
+        exit(1);
+    }
+
+    unsigned char word[8];
+    ssize_t bytesRead = read(pagemapFd, word, 8);
+    if (bytesRead < 0) {
+        perror("read on pagemap");
+        exit(1);
+    }
+
+    int softDirtyBit = getBit(word, 55);
+    return softDirtyBit;
+}
+
+int countSoftDirtyPages(unsigned int startAddress, unsigned int endAddress) {
+    if (startAddress % PAGE_SIZE != 0) {
+        fprintf(stderr, "countSoftDirtyPages: start address %u is not page-aligned\n",
+                startAddress);
+        exit(1);
+    }
+    if (endAddress % PAGE_SIZE != 0) {
+        fprintf(stderr, "countSoftDirtyPages: end address %u is not page-aligned\n",
+                endAddress);
+        exit(1);
+    }
+
+    unsigned int startPage = startAddress / PAGE_SIZE;
+    unsigned int endPage = endAddress / PAGE_SIZE;
+
+    int count = 0;
+    for (unsigned int page = startPage; page < endPage; page++) {
+        int isSoftDirty = checkPageSoftDirty(page);
+        count += isSoftDirty;
+    }
+    return count;
+}
 
 int main(int argc, char ** argv) {
     const char * usage = "usage: ./wsetracker target-pid";
@@ -19,18 +74,15 @@ int main(int argc, char ** argv) {
     strcpy(pagemapPath, "/proc/");
     strcat(pagemapPath, pidString);
     strcat(pagemapPath, "/pagemap");
-    printf("Pagemap path: %s\n", pagemapPath);
 
-    int pagemapFd = open(pagemapPath, O_RDONLY);
-    printf("pagemapFd: %d\n", pagemapFd);
-    off_t offset = lseek(pagemapFd, 8, SEEK_SET);
-    printf("offset: %ld\n", offset);
-    char word[8];
-    ssize_t bytesRead = read(pagemapFd, word, 8);
-    printf("bytes read: %ld\n", bytesRead);
-    for (int i = 0; i < 8; i++) {
-        printf("%d\n", (int)word[i]);
+    pagemapFd = open(pagemapPath, O_RDONLY);
+    if (pagemapFd < 0) {
+        perror("open pagemap");
+        exit(1);
     }
+
+    int softDirtyPages = countSoftDirtyPages(0xc0000000, 0xc0100000);
+    printf("Soft-dirty pages: %d\n", softDirtyPages);
 
     return 0;
 }
