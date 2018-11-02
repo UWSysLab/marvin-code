22d30f6ca6ee7f2ceb26f117525a77882508d2d8
Niel Lebeck <nl35@cs.washington.edu>
2018-11-02 14:25:24 -0700

    Add marking of page idle bits

--- pagemap-working-set-estimation/wsetracker.c
+++ pagemap-working-set-estimation/wsetracker.c
@@ -22,10 +22,15 @@ int getBit(unsigned char word[8], int index) {
     return (word[byteIndex] >> bitIndex) & 1;
 }
 
+void setBit(unsigned char word[8], int index) {
+    int byteIndex = index / 8;
+    int bitIndex = index % 8;
+    word[byteIndex] = word[byteIndex] | (1 << bitIndex);
+}
+
 uint64_t convertBytesToLongWord(unsigned char bytes[8]) {
     uint64_t word = 0;
     for (int i = 0; i < 8; i++) {
-        printf("%d: %u, %lu\n", i, bytes[i], ((uint64_t)bytes[i] << (i * 8)));
         word += ((uint64_t)bytes[i] << (i * 8));
     }
     return word;
@@ -33,9 +38,7 @@ uint64_t convertBytesToLongWord(unsigned char bytes[8]) {
 
 uint64_t getPfn(unsigned char pagemapEntry[8]) {
     uint64_t pagemapEntryWord = convertBytesToLongWord(pagemapEntry);
-    printf("pagemapEntryWord: %lu\n", pagemapEntryWord);
     uint64_t pfn = pagemapEntryWord & PFN_MASK;
-    printf("pfn: %lu\n", pfn);
     return pfn;
 }
 
@@ -50,6 +53,21 @@ void readLongWordBytes(int fd, uint64_t wordOffset, unsigned char output[8]) {
     ssize_t bytesRead = read(fd, output, 8);
     if (bytesRead < 0) {
         perror("read");
+        exit(1);
+    }
+}
+
+void writeLongWordBytes(int fd, uint64_t wordOffset, unsigned char input[8]) {
+    off_t offset = wordOffset * 8;
+    off_t retOffset = lseek(fd, offset, SEEK_SET);
+    if (retOffset < 0) {
+        perror("lseek");
+        exit(1);
+    }
+
+    ssize_t bytesWritten = write(fd, input, 8);
+    if (bytesWritten < 0) {
+        perror("write");
         exit(1);
     }
 }
@@ -71,12 +89,24 @@ int checkPageIdle(uint64_t page) {
     readLongWordBytes(idleBitmapFd, bitmapWordOffset, bitmapWordBytes);
 
     unsigned int bitOffset = pfn % 64;
-    //uint64_t bitmapWord = convertBytesToLongWord(bitmapWordBytes);
-    //int idleBit = (bitmapWord << bitOffset) & 1;
     int idleBit = getBit(bitmapWordBytes, bitOffset);
-    printf("idle bit: %d\n", idleBit);
 
-    return 0;
+    return idleBit;
+}
+
+void markPageIdle(uint64_t page) {
+    unsigned char word[8];
+    readLongWordBytes(pagemapFd, page, word);
+    uint64_t pfn = getPfn(word);
+
+    off_t bitmapWordOffset = pfn / 64;
+    unsigned char bitmapWordBytes[8];
+    readLongWordBytes(idleBitmapFd, bitmapWordOffset, bitmapWordBytes);
+
+    unsigned int bitOffset = pfn % 64;
+    setBit(bitmapWordBytes, bitOffset);
+
+    writeLongWordBytes(idleBitmapFd, bitmapWordOffset, bitmapWordBytes);
 }
 
 int countSoftDirtyPages(uint64_t startAddress, uint64_t endAddress) {
@@ -102,15 +132,61 @@ int countSoftDirtyPages(uint64_t startAddress, uint64_t endAddress) {
     return count;
 }
 
+int countNonIdlePages(uint64_t startAddress, uint64_t endAddress) {
+    if (startAddress % PAGE_SIZE != 0) {
+        fprintf(stderr, "countIdlePages: start address %lu is not page-aligned\n",
+                startAddress);
+        exit(1);
+    }
+    if (endAddress % PAGE_SIZE != 0) {
+        fprintf(stderr, "countIdlePages: end address %lu is not page-aligned\n",
+                endAddress);
+        exit(1);
+    }
+
+    uint64_t startPage = startAddress / PAGE_SIZE;
+    uint64_t endPage = endAddress / PAGE_SIZE;
+
+    int count = 0;
+    for (uint64_t page = startPage; page < endPage; page++) {
+        int isIdle = checkPageIdle(page);
+        count += !isIdle;
+    }
+    return count;
+}
+
+void markIdlePages(uint64_t startAddress, uint64_t endAddress) {
+    if (startAddress % PAGE_SIZE != 0) {
+        fprintf(stderr, "markIdlePages: start address %lu is not page-aligned\n",
+                startAddress);
+        exit(1);
+    }
+    if (endAddress % PAGE_SIZE != 0) {
+        fprintf(stderr, "markIdlePages: end address %lu is not page-aligned\n",
+                endAddress);
+        exit(1);
+    }
+
+    uint64_t startPage = startAddress / PAGE_SIZE;
+    uint64_t endPage = endAddress / PAGE_SIZE;
+
+    for (uint64_t page = startPage; page < endPage; page++) {
+        markPageIdle(page);
+    }
+}
+
 int main(int argc, char ** argv) {
-    const char * usage = "usage: ./wsetracker target-pid";
-    if (argc < 2) {
+    const char * usage = "usage: ./wsetracker target-pid start-addr end-addr r|w|c";
+    if (argc < 5) {
         fprintf(stderr, "%s\n", usage);
         return 1;
     }
 
     const char * pidString = argv[1];
-    printf("Using pid %s\n", pidString);
+    uint64_t startAddr = atoll(argv[2]);
+    uint64_t endAddr = atoll(argv[3]);
+    const char op = argv[4][0];
+    printf("Using pid %s, startAddr %lu, endAddr %lu, op %c\n", pidString, startAddr, endAddr, op);
 
     char pagemapPath[100];
     strcpy(pagemapPath, "/proc/");
@@ -125,16 +201,29 @@ int main(int argc, char ** argv) {
         exit(1);
     }
 
-    idleBitmapFd = open(idleBitmapPath, O_RDONLY);
+    idleBitmapFd = open(idleBitmapPath, O_RDWR);
     if (idleBitmapFd < 0) {
         perror("open idle bitmap");
         exit(1);
     }
 
-    int softDirtyPages = countSoftDirtyPages(0xc0000000, 0xc0100000);
-    printf("Soft-dirty pages: %d\n", softDirtyPages);
-
-    checkPageIdle(0xc0000000 / PAGE_SIZE);
+    int count = 0;
+    switch(op) {
+    case 'w':
+        count = countSoftDirtyPages(startAddr, endAddr);
+        printf("Soft-dirty pages: %d\n", count);
+        break;
+    case 'r':
+        count = countNonIdlePages(startAddr, endAddr);
+        printf("Non-idle pages: %d\n", count);
+        break;
+    case 'c':
+        markIdlePages(startAddr, endAddr);
+        printf("Marked pages idle.\n");
+        break;
+    default:
+        printf("Unrecognized op: %c\n", op);
+    }
 
     return 0;
 }
