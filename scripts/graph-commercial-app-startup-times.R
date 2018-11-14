a3db7b09bff7e16c1ed14e296d992612b144e593
Niel Lebeck <nl35@cs.washington.edu>
2018-11-14 14:42:50 -0800

    Clean up commercial app startup time graph

--- scripts/graph-commercial-app-startup-times.R
+++ scripts/graph-commercial-app-startup-times.R
@@ -1,3 +1,6 @@
+# I referred to this StackOverflow answer to figure out how to rotate the
+# x-axis labels: https://stackoverflow.com/a/1331400.
+
 library(ggplot2)
 
 myData = read.csv("commercial-app-startup-times.csv", header = TRUE)
@@ -6,5 +9,8 @@ pdf("commercial-app-startup-times.pdf")
 ggplot(myData) +
     geom_col(mapping = aes(x = Name, y = Time, fill = System), position = "dodge") +
     xlab("App") +
-    ylab("Average startup time (s)")
+    ylab("Average startup time (s)") +
+    labs(fill = "") +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    coord_fixed(0.5)
 dev.off()
