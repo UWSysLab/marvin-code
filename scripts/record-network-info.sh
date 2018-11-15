da726deb6ac940a481ce6b94e6a749d55f9e2947
Niel Lebeck <nl35@cs.washington.edu>
2018-11-15 11:24:31 -0800

    Make it easier to configure sleep times and duration

--- scripts/record-network-info.sh
+++ scripts/record-network-info.sh
@@ -3,7 +3,11 @@
 # This script is designed to be run on an Android device. It waits for a
 # process to start that has a name that is a superstring of the supplied
 # argument, and then it prints "bytes received" and "bytes transmitted" values
-# from /proc/net/dev at 0.1-second intervals for 10 seconds.
+# from /proc/net/dev at regular intervals for a configurable amount of time.
+
+WAIT_SLEEP_INTERVAL=0.1    # seconds
+OUTPUT_SLEEP_INTERVAL=0.1    # seconds
+OUTPUT_DURATION=150    # number of "output sleeps"
 
 USAGE="./record-network-info.sh app-name"
 
@@ -16,15 +20,15 @@ fi
 
 pid=$(ps | grep "$appName" | cut -d " " -f 5)
 while [[ $pid = "" ]] ; do
-    sleep 0.1
+    sleep $WAIT_SLEEP_INTERVAL
     pid=$(ps | grep "$appName" | cut -d " " -f 5)
 done
 
 echo "App $appName started with PID $pid"
 
 counter=0
-while [[ $counter -le 100 ]] ; do
+while [[ $counter -le $OUTPUT_DURATION ]] ; do
     cat /proc/net/dev | sed -n '4p' | tr -s ' ' | cut -d " " -f 3,11
     counter=$counter+1
-    sleep 0.1
+    sleep $OUTPUT_SLEEP_INTERVAL
 done
