725d1d23ecb4153dd46406198b5e8509a61d7f8c
Niel Lebeck <nl35@cs.washington.edu>
2018-11-21 14:43:03 -0800

    Add object size vs cumulative heap frac graph to master graphing script

--- scripts/generate-all-graphs.pl
+++ scripts/generate-all-graphs.pl
@@ -6,3 +6,6 @@ use strict;
 system("./graph-section-2-graph.R section-2-graph-data.csv section-2-graph.pdf");
 system("./graph-commercial-app-startup-times.R commercial-app-startup-times.csv commercial-app-startup-times.pdf");
 system("./graph-compiler-overhead-synthetic.R compiler-overhead-synthetic.csv compiler-overhead-synthetic.pdf");
+
+system("./process-object-size-heap-frac-data.pl > heapfrac-data.txt");
+system("./graph-object-size-heap-frac-graph.R heapfrac-data.txt cumulative-heap-frac-vs-object-size.pdf");
