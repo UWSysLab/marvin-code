25f7a2b0b6f8c7bca0f6130baadc98836fe01263
Niel Lebeck <nl35@cs.washington.edu>
2018-11-30 15:11:08 -0800

    Add code for drawing images in UI to verify them

--- apps/GlideApp/app/src/main/java/edu/washington/cs/nl35/glideapp/MainActivity.java
+++ apps/GlideApp/app/src/main/java/edu/washington/cs/nl35/glideapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.FutureTarget;
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private final String FILE_PREFIX = "image";
     private final String FILE_SUFFIX = ".png";
     private final int MAX_NUM_IMAGES = 500;
+    private final int VERIFY_IMAGES_SLEEP_TIME_MS = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,19 +43,32 @@ public class MainActivity extends AppCompatActivity {
         imageLoopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText numImagesEditText = findViewById(R.id.numImagesEditText);
-                String text = numImagesEditText.getText().toString();
-                int numImages = Integer.parseInt(text);
-                runImageLoopInBackground(numImages);
+                runImageLoopInBackground(getNumImages(), null);
             }
         });
+
+        Button verifyImagesButton = findViewById(R.id.verifyImagesButton);
+        verifyImagesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageView imageView = findViewById(R.id.imageView);
+                runImageLoopInBackground(getNumImages(), imageView);
+            }
+        });
+    }
+
+    private int getNumImages() {
+        EditText numImagesEditText = findViewById(R.id.numImagesEditText);
+        String text = numImagesEditText.getText().toString();
+        int numImages = Integer.parseInt(text);
+        return numImages;
     }
 
     /**
      * Create a placeholder file for each image file holding a single byte of data. Run this method
      * once before copying the actual image files into the app folder and opening them (apparently
-     * some Android security policy prevents apps from opening files in their local files directory
-     * that they did not create).
+     * some Android security policy prevents an app from opening a file in its local files directory
+     * if the app itself has not already created a file with that name).
      */
     private void touchFiles() {
         for (int i = 0; i < MAX_NUM_IMAGES; i++) {
@@ -74,16 +89,23 @@ public class MainActivity extends AppCompatActivity {
         return FILE_PREFIX + index + FILE_SUFFIX;
     }
 
-    private void runImageLoopInBackground(final int numImages) {
+    private void runImageLoopInBackground(final int numImages, final ImageView imageView) {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                runImageLoop(numImages);
+                runImageLoop(numImages, imageView);
             }
         }).start();
     }
 
-    private void runImageLoop(int numImages) {
+    /**
+     * Touch the images in the working set. This method has two different modes. If the ImageView is
+     * null, it runs exclusively in the background, loading each image and moving onto the next one
+     * as soon as possible. If the ImageView is non-null, it loads each image, then queues it for
+     * being drawn into the ImageView on the UI thread, then sleeps for a configurable amount of
+     * time.
+     */
+    private void runImageLoop(int numImages, final ImageView imageView) {
         Log.i(TAG, "Starting image loop over " + numImages + " images");
         long startTime = System.nanoTime();
         for (int i = 0; i < numImages; i++) {
@@ -91,6 +113,15 @@ public class MainActivity extends AppCompatActivity {
             FutureTarget<Drawable> futureTarget = Glide.with(this).load(imageFile).submit();
             try {
                 final Drawable drawable = futureTarget.get();
+                if (imageView != null) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            imageView.setImageDrawable(drawable);
+                        }
+                    });
+                    Thread.sleep(VERIFY_IMAGES_SLEEP_TIME_MS);
+                }
             } catch (ExecutionException e) {
                 handleException(e);
             } catch (InterruptedException e) {
