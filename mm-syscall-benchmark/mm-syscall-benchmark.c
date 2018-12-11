2901af068340b003a0d65a7ff31b24ffef009958
Niel Lebeck <nl35@cs.washington.edu>
2018-12-11 09:43:58 -0800

    Add simple C benchmark to measure munmap time

---
+++ mm-syscall-benchmark/mm-syscall-benchmark.c
@@ -0,0 +1,51 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/time.h>
+#include <time.h>
+
+int main(int argc, char ** argv) {
+    const char * USAGE_STR = "usage: ./mm-syscall-benchmark num-bytes";
+
+    if (argc != 2) {
+        fprintf(stderr, "%s\n", USAGE_STR);
+        return 1;
+    }
+
+    uint64_t numBytes = atol(argv[1]);
+    printf("numBytes: %lu\n", numBytes);
+
+    void * map = mmap((void *)0xc0000000, numBytes, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (map == (void *)-1) {
+        perror("mmap");
+        return 1;
+    }
+    printf("map: %lx\n", (uint64_t)map);
+
+    for (int i = 0; i < numBytes; i++) {
+        *((uint8_t *)map + i) = 42;
+    }
+
+    int ret;
+    struct timeval startTime;
+    ret = gettimeofday(&startTime, NULL);
+    if (ret < 0) {
+        perror("gettimeofday");
+        return -1;
+    }
+
+    munmap(map, numBytes);
+
+    struct timeval endTime;
+    ret = gettimeofday(&endTime, NULL);
+    if (ret < 0) {
+        perror("gettimeofday");
+        return -1;
+    }
+
+    uint64_t timeUs = (endTime.tv_sec - startTime.tv_sec) * (1000 * 1000) + (endTime.tv_usec - startTime.tv_usec);
+    printf("Time (us): %lu\n", timeUs);
+
+    return 0;
+}
