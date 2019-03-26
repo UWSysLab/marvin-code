806d1ecdfb86cc7b6020515c4ec70756cba3deb0
Niel Lebeck <nl35@cs.washington.edu>
2019-03-25 17:30:28 -0700

    Add simple C program to measure time to malloc and touch pages of memory

---
+++ malloc-timer/malloc-timer.c
@@ -0,0 +1,43 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+#ifndef PAGE_SIZE
+#define PAGE_SIZE 4096
+#endif
+
+int main(int argc, char ** argv) {
+    int ARRAY_SIZE = 1048576;
+    int NUM_ARRAYS = 180;
+    char DUMMY_VALUE = 42;
+
+    struct timespec startTime;
+    int ret = clock_gettime(CLOCK_MONOTONIC, &startTime);
+    if (ret < 0) {
+        perror("clock_gettime");
+        return -1;
+    }
+
+    char ** arrayPtrs = malloc(sizeof(char *) * NUM_ARRAYS);
+    for (int i = 0; i < NUM_ARRAYS; i++) {
+        char * array = malloc(ARRAY_SIZE);
+        for (int j = 0; j < ARRAY_SIZE; j += PAGE_SIZE) {
+            array[j] = DUMMY_VALUE;
+        }
+        arrayPtrs[i] = array;
+    }
+
+    struct timespec endTime;
+    ret = clock_gettime(CLOCK_MONOTONIC, &endTime);
+    if (ret < 0) {
+        perror("clock_gettime");
+        return -1;
+    }
+
+    int timeDiffMs =  (endTime.tv_sec - startTime.tv_sec) * 1000
+                    + (endTime.tv_nsec - startTime.tv_nsec) / (1000 * 1000);
+
+    printf("Allocated and touched the pages of %d arrays of size %d in %d ms (using page size %d)\n", NUM_ARRAYS, ARRAY_SIZE, timeDiffMs, PAGE_SIZE);
+}
