012b3a5d96890d7d433ce50c8a834250dded1823
Niel Lebeck <nl35@cs.washington.edu>
2019-03-27 10:59:30 -0700

    Change AllocatorActivity log-parsing script to check for clones becoming active once again

--- scripts/parse-allocatoractivity-log.pl
+++ scripts/parse-allocatoractivity-log.pl
@@ -1,8 +1,15 @@
 #!/usr/bin/perl
 
-# An updated version of parse-microbenchmark-log.pl that only parses the
-# messages common to all apps and checks to see if app clones have become
-# "inactive" (have not printed a log message within some timeout interval).
+# An updated version of parse-microbenchmark-log.pl that is tailored to the
+# MemoryWaster AllocatorActivity and observes when app clones become "inactive"
+# and "active." Intuitively, an app clone is considered inactive if it becomes
+# too slow to perform work or print log messages reflecting that work. Here,
+# an AllocatorActivity clone is considered inactive if it has not performed
+# bucket deletion within a given timeout interval, and the app clone is
+# considered active once again if it performs bucket deletion within a
+# (possibly smaller) "active threshold" interval. Active/inactive detection
+# relies on the AllocatorActivity performing bucket deletion at some regular
+# interval.
 
 use warnings;
 use strict;
@@ -10,11 +17,16 @@ use strict;
 use lib '.';
 use NielAndroidUtils qw(parseLogMessage calcTimeDiffSeconds);
 
+my $STATUS_ACTIVE = 0;
+my $STATUS_INACTIVE = 1;
+
 my $INACTIVE_TIMEOUT = 20; # seconds
+my $ACTIVE_THRESHOLD = 7; # seconds
 my $CLONE_NAME = "edu.washington.cs.nl35.memorywaster";
 
 my $startTime = "";
 my %pidTimes;
+my %pidStatuses;
 
 while(<>) {
     my @parsedMessage = parseLogMessage($_);
@@ -29,6 +41,7 @@ while(<>) {
             my $startedPid = $1;
             my $cloneNum = $2;
             $pidTimes{$startedPid} = $time;
+            $pidStatuses{$startedPid} = $STATUS_ACTIVE;
             print("$time\t$startedPid\t$cloneNum\tstart\n");
         }
         if ($msg =~ /^Process $CLONE_NAME(\d+) \(pid (\d+)\) has died$/) {
@@ -44,14 +57,26 @@ while(<>) {
             my $cloneNum = $1;
             print("$time\t-\t$cloneNum\tstart-intent\n");
         }
-        if (exists $pidTimes{$pid}) {
+        if ($msg =~ /^Deleted \d+ buckets starting at \d+ with stride \d+$/) {
+            if (!exists($pidTimes{$pid})) {
+                print(STDERR "ERROR: startup of clone with PID $pid was not recorded\n");
+            }
+
+            # Check if this process has become active
+            my $timeDiff = calcTimeDiffSeconds($pidTimes{$pid}, $time);
+            if ($timeDiff < $ACTIVE_THRESHOLD && $pidStatuses{$pid} == $STATUS_INACTIVE) {
+                $pidStatuses{$pid} = $STATUS_ACTIVE;
+                print("$time\t$pid\t-\tactive\n");
+            }
+
             $pidTimes{$pid} = $time;
         }
 
+        # Check if any processes have become inactive
         for my $pid (keys %pidTimes) {
             my $timeDiff = calcTimeDiffSeconds($pidTimes{$pid}, $time);
-            if ($timeDiff > $INACTIVE_TIMEOUT) {
-                delete($pidTimes{$pid});
+            if ($timeDiff >= $INACTIVE_TIMEOUT && $pidStatuses{$pid} == $STATUS_ACTIVE) {
+                $pidStatuses{$pid} = $STATUS_INACTIVE;
                 print("$time\t$pid\t-\tinactive\n");
             }
         }
