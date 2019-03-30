c07286f322f4dd6991a50c3e2a65f74e3a4c3d27
Niel Lebeck <nl35@cs.washington.edu>
2019-03-30 14:23:12 -0700

    Add script to graph Android memory allocation data

---
+++ scripts/graph-android-allocation-data.R
@@ -0,0 +1,28 @@
+#!/usr/bin/Rscript
+
+# This script expects as input a single file produced by
+# process-memory-data.pl.
+#
+# I used this StackOverflow answer to learn how to remove the legend title from
+# a graph: https://stackoverflow.com/a/14771584.
+
+library(ggplot2)
+
+args = commandArgs(trailingOnly=TRUE)
+if (length(args) < 2) {
+    stop("usage: ./graph-android-allocation-data.R input-csv output-pdf")
+}
+inputFile = args[1]
+outputFile = args[2]
+
+myData = read.csv(inputFile, header = TRUE)
+
+pdf(outputFile)
+ggplot(myData) +
+    geom_line(mapping = aes(x = TranslatedTimestamp, y = TranslatedRSSMB, color = WorkingSet, linetype = WorkingSet), size = 1) +
+    xlab("Time (ms)") +
+    ylab("Memory allocated (MB)") +
+    theme_classic() +
+    theme(axis.text = element_text(size=16), axis.title = element_text(size=16), legend.text = element_text(size=16), legend.title = element_blank()) +
+    coord_fixed(10)
+dev.off()
